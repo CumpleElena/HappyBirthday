22ecb8fb6a8a17e15343071bdd871d6de6ae4dd5
jonathan <jonathandyallo@gmail.com>
2021-06-12 17:51:55 -0300

    script

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function clickeame() {
+    alert('Tus regalitos estan en el boton de abajo uwu <3')
+}
+
+function clickeame2() {
+    alert('Te amo muchito mi vida <3333 Feliz cumple uwu!')
+}
+
+function clickeame3() {
+    alert('Regalitos: https://drive.google.com/drive/folders/147a-4iFuieH119ejtD51E9a8Bc6gCUME?usp=sharing')
+}
